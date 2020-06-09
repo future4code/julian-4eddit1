dd7ca8b23d3a60fd38227cdbab3cfc84d052064d
Nathália Constantino Costa <n.athiccosta@gmail.com>
2020-06-09 14:13:12 -0300

    Rotas

--- lab-eddit/src/App.js
+++ lab-eddit/src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
-import PageLogin from './pages/PageLogin'
-import PageListPosts from './pages/PageListPosts'
+import PageLogin from './pages/PageLogin';
+import PageListPosts from './pages/PageListPosts';
+import PageCadastro from './pages/PageCadastro';
+import PagePost from './pages/PagePost';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/login">
+          <PageLogin/>
+        </Route>
+        <Route exact path="/cadaster">
+          <PageCadastro/>
+        </Route>
+        <Route exact path="/feed">
+          <PageListPosts/>
+        </Route>
+        <Route exact path="/:post">
+          <PagePost/>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 

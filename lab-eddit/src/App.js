582b5b0a8f1c453fc4f5b66aa391427965d91a84
nilsonkuroki <nilsonkurokii@gmail.com>
2020-06-10 17:10:26 -0300

    requisicao para pegar posts feita

--- lab-eddit/src/App.js
+++ lab-eddit/src/App.js
@@ -1,28 +1,44 @@
-import React from 'react';
+import React, {useReducer, useState, useEffect} from 'react';
 import PageLogin from './pages/PageLogin';
 import PageListPosts from './pages/PageListPosts';
 import PageCadastro from './pages/PageCadastro';
 import PagePost from './pages/PagePost';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import {PostsContext} from './Context/index'
+import axios from 'axios'
+
+const initialState = {
+  post: []
+}
+
+const reducer = (state,action)=>{
+  switch(action.type){
+    default: 
+    return state
+  }
+}
 
 const App = () => {
+
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/login">
-          <PageLogin/>
-        </Route>
-        <Route exact path="/cadaster">
-          <PageCadastro/>
-        </Route>
-        <Route exact path="/feed">
-          <PageListPosts/>
-        </Route>
-        <Route exact path="/:post">
-          <PagePost/>
-        </Route>
-      </Switch>
-    </BrowserRouter>
+    <PostsContext.Provider value={null}>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <PageLogin/>
+          </Route>
+          <Route exact path="/cadaster">
+            <PageCadastro/>
+          </Route>
+          <Route exact path="/feed">
+            <PageListPosts/>
+          </Route>
+          <Route exact path="/:post">
+            <PagePost/>
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </PostsContext.Provider>
   );
 }
 
--- lab-eddit/src/pages/PageListPosts/index.js
+++ lab-eddit/src/pages/PageListPosts/index.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, {useEffect, useState}from 'react'
 import styled from 'styled-components'
+import {useHistory} from 'react-router-dom'
 import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
 import Post from '../PageListPosts/post'
 import NewPost from '../PageListPosts/NewPost'
+import axios from 'axios'
 
 const ContainerPageListPosts = styled.div`
     display: flex;
@@ -14,12 +16,40 @@ const ContainerPageListPosts = styled.div`
 `
 
 const PageListPosts = ()=>{
+    const token = localStorage.getItem('token')
+    const [posts, setPosts] = useState([])
+    const history = useHistory()
+    
+    useEffect(()=>{
+        if (token === null) {
+            history.push("/");
+        }
+
+        axios.get('https://us-central1-labenu-apis.cloudfunctions.net/labEddit/posts',{
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: token
+            }
+        }).then((response)=>{
+            console.log(response.data.posts)
+            setPosts(response.data.posts)
+        })
+
+    },[])
+
     return (
     <ContainerPageListPosts>
         <NewPost/>
-        <Post/>
-        <Post/>
-        <Post/>
+        {posts.map((post)=>{
+            return(
+                <div>
+                    <p>{post.name}</p>
+                    <Post/>
+                </div>
+                
+            )
+        })}
+        
     </ContainerPageListPosts>
     )
 }
